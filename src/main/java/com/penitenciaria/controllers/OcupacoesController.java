f6075ed5b624933ab4364702cd54a1cd70b8a58a
MarlonAngonese <marlonmaster2020@gmail.com>
2021-07-06 17:51:53 -0300

    Set complete CRUD for Ocupacoes

--- src/main/java/com/penitenciaria/controllers/OcupacoesController.java
+++ src/main/java/com/penitenciaria/controllers/OcupacoesController.java
@@ -2,6 +2,7 @@ package com.penitenciaria.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,19 +16,22 @@ public class OcupacoesController {
 	@Autowired
 	private OcupacoesRepository or;
 	
+	//Adicionar - Get
 	@RequestMapping(value="/ocupacoes/add", method=RequestMethod.GET)
 	public String form() {
 		return "Ocupacoes/addOcupacoes";
 	}
 	
+	//Adicionar - Post
 	@RequestMapping(value="/ocupacoes/add", method=RequestMethod.POST)
 	public String form(Ocupacoes ocupacao) {
 		
 		or.save(ocupacao);
 		
-		return "redirect:/ocupacoes/add";
+		return "redirect:/ocupacoes";
 	}
 	
+	//Index - Listagem de Registros
 	@RequestMapping(value="/ocupacoes", method=RequestMethod.GET)
 	public ModelAndView listaOcupacoes() {
 		ModelAndView mv = new ModelAndView("Ocupacoes/indexOcupacoes");
@@ -37,5 +41,32 @@ public class OcupacoesController {
 		mv.addObject("ocupacoes", ocupacoes);
 		
 		return mv;
+	}
+	
+	//Editar - Get
+	@RequestMapping(value="/ocupacoes/edit/{codigo}", method=RequestMethod.GET)
+	public ModelAndView editarOcupacoes(@PathVariable("codigo") long codigo) {
+		Ocupacoes ocupacao = or.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("Ocupacoes/editOcupacoes");
+		mv.addObject("ocupacao", ocupacao);
+		return mv;
+	}
+	
+	//Editar - Post
+	@RequestMapping(value="/ocupacoes/edit/{codigo}", method=RequestMethod.POST)
+	public String editarOcupacoesP(Ocupacoes ocupacao, @PathVariable("codigo") long codigo) {
+		ocupacao.setCodigo(codigo);
+		or.save(ocupacao);
+		return "redirect:/ocupacoes";
+	}
+	
+	//Excluir (Deletar)
+	@RequestMapping(value="/ocupacoes/delete/{codigo}", method=RequestMethod.GET)
+	public String deletarOcupacoes(@PathVariable("codigo") long codigo) {
+		Ocupacoes ocupacao = or.findByCodigo(codigo);
+		
+		or.delete(ocupacao);
+		
+		return "redirect:/ocupacoes";
 	}
 }
--- src/main/java/com/penitenciaria/repository/OcupacoesRepository.java
+++ src/main/java/com/penitenciaria/repository/OcupacoesRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.penitenciaria.models.Ocupacoes;
 
 public interface OcupacoesRepository extends CrudRepository<Ocupacoes, String> {
-
+	Ocupacoes findByCodigo(long codigo);
 }
