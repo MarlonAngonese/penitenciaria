c9b6a0bd7e60c085aa386385cc26578a25c0d4fb
unknown <renanandreolla@outlook.com>
2021-07-06 22:11:32 -0300

    Policiais - Struct

--- src/main/java/com/penitenciaria/controllers/SentencasController.java
+++ src/main/java/com/penitenciaria/controllers/SentencasController.java
@@ -2,8 +2,10 @@ package com.penitenciaria.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.penitenciaria.models.Sentencas;
 import com.penitenciaria.repository.SentencasRepository;
@@ -24,6 +26,41 @@ public class SentencasController {
 		
 		or.save(sentenca);
 		
-		return "redirect:/sentencas/add";
+		return "redirect:/sentencas";
+	}
+	
+	@RequestMapping(value="/sentencas", method=RequestMethod.GET)
+	public ModelAndView listaSentencas() {
+		ModelAndView mv = new ModelAndView("Sentencas/indexSentencas");
+		
+		Iterable<Sentencas> sentencas = or.findAllByOrderByCodigoDesc();
+		
+		mv.addObject("sentencas", sentencas);
+		
+		return mv;
+	}
+
+	@RequestMapping(value="/sentencas/edit/{codigo}", method=RequestMethod.GET)
+	public ModelAndView editarSentencas(@PathVariable("codigo") long codigo) {
+		Sentencas sentenca = or.findByCodigo(codigo);
+		ModelAndView mv = new ModelAndView("Sentencas/editSentencas");
+		mv.addObject("sentenca", sentenca);
+		return mv;
+	}
+	
+	@RequestMapping(value="/sentencas/edit/{codigo}", method=RequestMethod.POST)
+	public String editarSentencasP(Sentencas sentenca, @PathVariable("codigo") long codigo) {
+		sentenca.setCodigo(codigo);
+		or.save(sentenca);
+		return "redirect:/sentencas";
+	}
+	
+	@RequestMapping(value="/sentencas/delete/{codigo}", method=RequestMethod.GET)
+	public String deletarSentencas(@PathVariable("codigo") long codigo) {
+		Sentencas sentenca = or.findByCodigo(codigo);
+		
+		or.delete(sentenca);
+		
+		return "redirect:/sentencas";
 	}
 }
--- src/main/java/com/penitenciaria/repository/SentencasRepository.java
+++ src/main/java/com/penitenciaria/repository/SentencasRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.penitenciaria.models.Sentencas;
 
 public interface SentencasRepository extends CrudRepository<Sentencas, String> {
+	Sentencas findByCodigo(long codigo);
 
+	Iterable<Sentencas> findAllByOrderByCodigoDesc();
 }
